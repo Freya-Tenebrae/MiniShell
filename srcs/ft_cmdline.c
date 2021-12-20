996e0961df36a36d60bf1acaf2582276d759ae87
Gauthier DENEUX <gadeneux@MacBook-Pro-de-Gauthier.local>
2021-12-20 02:09:15 +0100

    debut des quotes uniquement ", le reste bientot

--- srcs/ft_cmdline.c
+++ srcs/ft_cmdline.c
@@ -6,7 +6,7 @@
 /*   By: gadeneux <gadeneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 00:54:38 by gadeneux          #+#    #+#             */
-/*   Updated: 2021/12/20 01:01:41 by gadeneux         ###   ########.fr       */
+/*   Updated: 2021/12/20 02:03:52 by gadeneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,34 @@
 
 t_elem	*ft_runcmd_next(t_elem *elem, char **envp, char **infile)
 {
-	char *cmd = 0;
+	char **cmd_args = 0;
 	
 	// Skip until argument
 	while (elem && elem->type != ARGUMENT)
 		elem = elem->next;
+	
+	int len = 0;
+	t_elem *cur = elem;
+	while (cur && cur->type == ARGUMENT)
+	{
+		len++;
+		cur = cur->next;
+	}
+	
+	cmd_args = malloc(sizeof(char*) * (len + 1));
+	if (!cmd_args)
+		return (0);
 		
+	int i = 0;
 	while (elem && elem->type == ARGUMENT)
 	{
-		ft_writestr_on(&cmd, elem->str);
-		if (elem->next)
-			ft_writechar_on(&cmd, (char) 32);
+		cmd_args[i] = ft_strdup(elem->str);
 		elem = elem->next;
+		i++;
 	}
-	printf("Command: ~%s~\n", cmd);
-	char *out = ft_runcmd(ft_getpath(envp), cmd, *infile);
-	printf("\"%s\"\n", out);
+	cmd_args[i] = NULL;
+	char *out = ft_runcmd(ft_getpath(envp), cmd_args, *infile);
+	printf("%s", out);
 	free(*infile);
 	*infile = out;
 	return (elem);
--- srcs/ft_minishell.c
+++ srcs/ft_minishell.c
@@ -6,70 +6,70 @@
 /*   By: gadeneux <gadeneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 12:44:43 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/12/20 01:55:46 by gadeneux         ###   ########.fr       */
+/*   Updated: 2021/12/20 02:04:21 by gadeneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_minishell.h"
 
-int		main(int ac, char **av, char **envp)
-{
-	(void) ac;
-	(void) av;
-	(void) envp;
-	
-	char **cmd_args = malloc(sizeof(char*) * 3);
-	cmd_args[0] = ft_strdup("ls");
-	cmd_args[1] = ft_strdup("-la");
-	cmd_args[2] = NULL;
-	ft_execcmd(ft_getpath(envp), cmd_args);
-	return (0);
-}
-
-// int	main(int ac, char **av, char **envp)
+// int		main(int ac, char **av, char **envp)
 // {
-// 	char	*str;
-// 	char	*pwd;
-// 	int		res_gnl;
-
 // 	(void) ac;
 // 	(void) av;
 // 	(void) envp;
-// 	while (42 == 42)
-// 	{
-// 		pwd = NULL;
-// 		pwd = getcwd(pwd, 0);
-// 		ft_putstr("minishell~ ");
-// 		ft_putstr(pwd);
-// 		free(pwd);
-// 		ft_putstr(" > ");
-// 		res_gnl = get_next_line(0, &str);
-// 		if (res_gnl == -1)
-// 			return (-1);
-// 		if (ft_strcmp("exit", str) == 0)
-// 		{
-// 			free(str);
-// 			return (1);
-// 		}
-// 		ft_putstr(str);
-// 		ft_putstr("\n");
-// 		t_elem *list = ft_readcmd(str);
-// 		if (!list)
-// 			return (1);
-// 		t_elem *cursor = list;
-		
-// 		while (cursor)
-// 		{
-// 			printf("%-3d ~%s~\n", cursor->type, cursor->str);
-// 			cursor = cursor->next;
-// 		}
-		
-// 		cursor = list;
-// 		char *infile = 0;
-// 		while ((cursor = ft_runcmd_next(cursor, envp, &infile)))
-// 			;
-// 		free(infile);
-// 		free(str);
-// 	}
+	
+// 	char **cmd_args = malloc(sizeof(char*) * 3);
+// 	cmd_args[0] = ft_strdup("ls");
+// 	cmd_args[1] = ft_strdup("-la");
+// 	cmd_args[2] = NULL;
+// 	ft_execcmd(ft_getpath(envp), cmd_args);
 // 	return (0);
 // }
+
+int	main(int ac, char **av, char **envp)
+{
+	char	*str;
+	char	*pwd;
+	int		res_gnl;
+
+	(void) ac;
+	(void) av;
+	(void) envp;
+	while (42 == 42)
+	{
+		pwd = NULL;
+		pwd = getcwd(pwd, 0);
+		ft_putstr("minishell~ ");
+		ft_putstr(pwd);
+		free(pwd);
+		ft_putstr(" > ");
+		res_gnl = get_next_line(0, &str);
+		if (res_gnl == -1)
+			return (-1);
+		if (ft_strcmp("exit", str) == 0)
+		{
+			free(str);
+			return (1);
+		}
+		// ft_putstr(str);
+		// ft_putstr("\n");
+		t_elem *list = ft_readcmd(str);
+		if (!list)
+			return (1);
+		t_elem *cursor = list;
+		
+		// while (cursor)
+		// {
+		// 	printf("%-3d ~%s~\n", cursor->type, cursor->str);
+		// 	cursor = cursor->next;
+		// }
+		
+		cursor = list;
+		char *infile = 0;
+		while ((cursor = ft_runcmd_next(cursor, envp, &infile)))
+			;
+		free(infile);
+		free(str);
+	}
+	return (0);
+}
