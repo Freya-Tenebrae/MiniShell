00de41ceea8dc39ad9a52a7662ffe41c82f24870
Celia Maginot <celia.maginot@orange.fr>
2021-12-22 17:54:42 +0100

    update signal ???

--- srcs/ft_signal_handler.c
+++ srcs/ft_signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 15:09:57 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/12/22 17:06:58 by cmaginot         ###   ########.fr       */
+/*   Updated: 2021/12/22 17:50:51 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ void	ft_signal_handler(int signal)
 {
 	if (signal == SIGINT)
 	{
-		ft_putstr("\b\b  ");
+		ft_putstr_fd("\b\b  ", 1);
 		ft_putstr("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		ft_put_prompt();
 	}
 	else if (signal == SIGQUIT)
-		ft_putstr("\b\b  \b\b");
+		ft_putstr_fd("\b\b  \b\b", 1);
 }
 
 void	ft_init_signal_handling(void)
