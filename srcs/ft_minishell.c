3fb1dc0259c51b32bc3b19352766d7b6a6045363
Gauthier DENEUX <gadeneux@MacBook-Pro-de-Gauthier.local>
2021-12-22 18:16:12 +0100

    ajout du main de celia

--- srcs/ft_minishell.c
+++ srcs/ft_minishell.c
@@ -6,80 +6,68 @@
 /*   By: gadeneux <gadeneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 12:44:43 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/12/22 18:06:08 by gadeneux         ###   ########.fr       */
+/*   Updated: 2021/12/22 18:15:48 by gadeneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_minishell.h"
 
-int	main(int ac, char **av, char **envp)
+static int	ft_act(char **str, char **envp)
 {
-	char	*str;
-	char	*pwd;
-	int		res_gnl;
+	int ret;
+	t_elem *list;
+	char *infile;
 
-	(void) ac;
-	(void) av;
-	(void) envp;
-	g_minishell_status = 0;
-	ft_init_signal_handling();
+	if (ft_strcmp("exit", *str) == 0)
+	{
+		free(*str);
+		return (1);
+	}
+	ret = 0;
+	list = ft_readcmd(*str, &ret);
+	if (ret == READ_OK)
+	{
+		infile = NULL;
+		while (list)
+			list = ft_runcmd_next(list, envp, &infile);
+		free(infile);
+	}
+	else if (ret == READ_QUOTE_ERR) 
+		printf("quote error.\n");
+	else
+	{
+		printf("error.\n");
+		return (1);
+	}
+	free(*str);
+	return (0);
+}
+
+static void	ft_loop(char **envp)
+{
+	int		res_gnl;
+	char	*str;
+
 	while (42 == 42)
 	{
-		pwd = NULL;
-		pwd = getcwd(pwd, 0);
-		ft_putstr("minishell~ ");
-		ft_putstr(pwd);
-		free(pwd);
-		ft_putstr(" > ");
+		ft_put_prompt();
 		res_gnl = get_next_line(0, &str);
-		if (res_gnl == 0)
+		if (res_gnl <= 0)
 		{
-			free(str);
-			return (1);
-		}
-		else if (res_gnl == -1)
-			return (-1);
-		if (g_minishell_status == 0)
-		{
-			if (ft_strcmp("exit", str) == 0
-			|| ft_strcmp("\\q", str) == 0)
-			{
+			if (res_gnl == 0)
 				free(str);
-				return (1);
-			}
-			int ret = 0;
-			t_elem *list = ft_readcmd(str, &ret);
-			if (ret == READ_OK)
-			{
-				t_elem *cursor = list;
-				
-				while (cursor)
-				{
-					printf("%-3d ~ %s ~\n", cursor->type, cursor->str);
-					cursor = cursor->next;
-				}
-				
-				// cursor = list;
-				// char *infile = 0;
-				// while ((cursor = ft_runcmd_next(cursor, envp, &infile)))
-				// 	;
-				// free(infile);
-				
-			} else if (ret == READ_QUOTE_ERR) {
-				printf("quote error.\n");
-			} else {
-				printf("error.\n");
-			}
-			free(str);
+			break;
 		}
-		else if (g_minishell_status == 2)
-		{
-			return (1);
-		}
-		else
-		{
-			g_minishell_status = 0;
-		}
+		if (ft_act(&str, envp) != 0)
+				break;
 	}
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	(void) ac;
+	(void) av;
+	ft_init_signal_handling();
+	ft_loop(envp);
 	return (0);
 }
