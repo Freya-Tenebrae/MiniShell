ee822408d05c456c89448eaa34c6be76e5dc17ca
Celia Maginot <celia.maginot@orange.fr>
2022-02-20 19:20:13 +0100

    add back deleted files

---
+++ srcs/ft_signal_handler.c
@@ -0,0 +1,25 @@
+
+#include "../includes/ft_minishell.h"
+
+static void	ft_signal_handler(int signal)
+{
+	if (signal == SIGINT)
+	{
+		ft_putstr_fd("\b\b  ", STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		if (g_status_minishell <= 0)
+			rl_redisplay();
+	}
+	else if (signal == SIGQUIT)
+	{
+		ft_putstr_fd("\b\b  \b\b", STDOUT_FILENO);
+	}
+}
+
+void	ft_init_signal_handling(void)
+{
+	signal(SIGINT, ft_signal_handler);
+	signal(SIGQUIT, ft_signal_handler);
+}
