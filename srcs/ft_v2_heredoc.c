d50b57980a8ff19bb1c67cd3f265716686efbbe7
Gauthier DENEUX <gadeneux@MacBook-Pro-de-Gauthier.local>
2022-03-15 12:33:50 +0100

    normed v2_heredoc

--- srcs/ft_v2_heredoc.c
+++ srcs/ft_v2_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_v2_heredoc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gadeneux <gadeneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 17:01:22 by gadeneux          #+#    #+#             */
-/*   Updated: 2022/03/11 15:37:17 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/03/15 12:27:24 by gadeneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,39 @@ static int	ft_is_eof(char *buffer, char *eof)
 	return (ret);
 }
 
+int	test(t_data **data, t_elem *list, int eof_ret, char **buffer)
+{
+	if (!ft_havequote(list->next->str))
+		ft_expension_on_heredoc(data, buffer);
+	if (eof_ret == -1)
+		return (ft_put_error(GENERIC_ERROR, "malloc error"));
+	if (!eof_ret)
+	{
+		ft_str_writeon(&(list->in_content), *buffer);
+		if (list->in_content == NULL)
+		{
+			free(buffer);
+			return (ft_put_error(GENERIC_ERROR, "malloc error"));
+		}
+		free(buffer);
+		ft_str_writeon(&(list->in_content), "\n");
+		if (list->in_content == NULL)
+			return (ft_put_error(GENERIC_ERROR, "malloc error"));
+	}
+	else
+	{
+		free(buffer);
+		return (1);
+	}
+	return (0);
+}
+
 int	ft_redirection_read_heredoc(t_data **data, t_elem *list)
 {
 	char	*buffer;
-	int		eof_ret;
+	int		ret;
 
 	buffer = NULL;
-	eof_ret = 0;
 	if (list->type == DOUBLE_IN)
 	{
 		while (1)
@@ -49,31 +75,12 @@ int	ft_redirection_read_heredoc(t_data **data, t_elem *list)
 				list->in_content = ft_strdup("");
 				ft_put_error(GENERIC_ERROR, \
 									"warning : «here-doc» reach end of file");
-				break ;
+				return (1);
 			}
-			eof_ret = ft_is_eof(buffer, list->next->str);
-			if (!ft_havequote(list->next->str))
-				ft_expension_on_heredoc(data, &buffer);
-			if (eof_ret == -1)
-				return (ft_put_error(GENERIC_ERROR, "malloc error"));
-			if (!eof_ret)
-			{
-				ft_str_writeon(&(list->in_content), buffer);
-				if (list->in_content == NULL)
-				{
-					free(buffer);
-					return (ft_put_error(GENERIC_ERROR, "malloc error"));
-				}
-				free(buffer);
-				ft_str_writeon(&(list->in_content), "\n");
-				if (list->in_content == NULL)
-					return (ft_put_error(GENERIC_ERROR, "malloc error"));
-			}
-			else
-			{
-				free(buffer);
-				break ;
-			}
+			ret = ft_is_eof(buffer, list->next->str);
+			ret = test(data, list, ret, &buffer);
+			if (ret != 0)
+				return (ret);
 		}
 	}
 	return (1);
